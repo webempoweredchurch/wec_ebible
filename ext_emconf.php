bcfe8e485738851e88b2a685abfb8b823f7fc953
Christoph Koehler <christoph@webempoweredchurch.org>
2009-12-15 20:44:36 +0000

    update version info after submission to TER.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "wec_ebible"
+# Extension Manager/Repository config file for ext "wec_ebible".
 #
-# Auto generated 01-07-2009 10:43
+# Auto generated 15-12-2009 14:43
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'Christian Technology Ministries International Inc.',
-	'version' => '1.0.4',
+	'version' => '1.0.5',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:32:{s:9:"CHANGELOG";s:4:"098f";s:26:"class.tx_wecebible_api.php";s:4:"c58d";s:32:"class.tx_wecebible_domainmgr.php";s:4:"7671";s:36:"class.tx_wecebible_itemsProcFunc.php";s:4:"6f99";s:12:"ext_icon.gif";s:4:"da4e";s:17:"ext_localconf.php";s:4:"6a99";s:14:"ext_tables.php";s:4:"d526";s:14:"ext_tables.sql";s:4:"8e6d";s:16:"locallang_db.xml";s:4:"a8a7";s:29:"csh/locallang_csh_feusers.xml";s:4:"2b15";s:25:"csh/locallang_csh_pi1.xml";s:4:"184f";s:14:"doc/manual.sxw";s:4:"8f9e";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"9250";s:14:"mod1/index.php";s:4:"67fa";s:18:"mod1/locallang.xml";s:4:"07ae";s:22:"mod1/locallang_mod.xml";s:4:"9cbc";s:19:"mod1/moduleicon.gif";s:4:"738b";s:14:"pi1/ce_wiz.gif";s:4:"1f46";s:30:"pi1/class.tx_wecebible_pi1.php";s:4:"f9d3";s:38:"pi1/class.tx_wecebible_pi1_wizicon.php";s:4:"a99c";s:19:"pi1/flexform_ds.xml";s:4:"4c96";s:17:"pi1/locallang.xml";s:4:"e85b";s:14:"pi2/ce_wiz.gif";s:4:"7a47";s:30:"pi2/class.tx_wecebible_pi2.php";s:4:"8b23";s:38:"pi2/class.tx_wecebible_pi2_wizicon.php";s:4:"eb9b";s:17:"pi2/locallang.xml";s:4:"5895";s:20:"pi2/translation.tmpl";s:4:"11e0";s:19:"res/ebibleicious.js";s:4:"e5ac";s:14:"res/styles.css";s:4:"91c0";s:20:"static/constants.txt";s:4:"babe";s:16:"static/setup.txt";s:4:"31e0";}',
+	'_md5_values_when_last_written' => 'a:32:{s:9:"CHANGELOG";s:4:"098f";s:26:"class.tx_wecebible_api.php";s:4:"c58d";s:32:"class.tx_wecebible_domainmgr.php";s:4:"7671";s:36:"class.tx_wecebible_itemsProcFunc.php";s:4:"6f99";s:12:"ext_icon.gif";s:4:"da4e";s:17:"ext_localconf.php";s:4:"6a99";s:14:"ext_tables.php";s:4:"d526";s:14:"ext_tables.sql";s:4:"8e6d";s:16:"locallang_db.xml";s:4:"a8a7";s:29:"csh/locallang_csh_feusers.xml";s:4:"2b15";s:25:"csh/locallang_csh_pi1.xml";s:4:"184f";s:14:"doc/manual.sxw";s:4:"8f9e";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"9250";s:14:"mod1/index.php";s:4:"df62";s:18:"mod1/locallang.xml";s:4:"07ae";s:22:"mod1/locallang_mod.xml";s:4:"9cbc";s:19:"mod1/moduleicon.gif";s:4:"738b";s:14:"pi1/ce_wiz.gif";s:4:"1f46";s:30:"pi1/class.tx_wecebible_pi1.php";s:4:"f9d3";s:38:"pi1/class.tx_wecebible_pi1_wizicon.php";s:4:"a99c";s:19:"pi1/flexform_ds.xml";s:4:"4c96";s:17:"pi1/locallang.xml";s:4:"e85b";s:14:"pi2/ce_wiz.gif";s:4:"7a47";s:30:"pi2/class.tx_wecebible_pi2.php";s:4:"8b23";s:38:"pi2/class.tx_wecebible_pi2_wizicon.php";s:4:"eb9b";s:17:"pi2/locallang.xml";s:4:"5895";s:20:"pi2/translation.tmpl";s:4:"11e0";s:19:"res/ebibleicious.js";s:4:"e5ac";s:14:"res/styles.css";s:4:"91c0";s:20:"static/constants.txt";s:4:"babe";s:16:"static/setup.txt";s:4:"31e0";}',
 	'suggests' => array(
 	),
 );
