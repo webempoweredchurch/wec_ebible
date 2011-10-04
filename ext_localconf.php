1a5a063fb414166c8cda619895d0f29608e436d7
Jeff Segars <jsegars@alumni.rice.edu>
2011-10-04 13:06:04 -0500

    Remove Verse of the Day functionality that is no longer offered by eBible.com

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,5 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_wecebible_pi1.php','_pi1','list_type',0);
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi2/class.tx_wecebible_pi2.php','_pi2','list_type',0);
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['sr_feuser_register']['extendingTCA'][] = $_EXTKEY;
--- ext_tables.php
+++ ext_tables.php
@@ -9,20 +9,14 @@ if (TYPO3_MODE=='BE')    {
 
 /* Set up the tt_content fields for the frontend plugin */
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages,recursive';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi2']='layout,select_key,pages,recursive';
 
 /* Adds the plugins and flexforms to the TCA */
-t3lib_extMgm::addPlugin(Array('LLL:EXT:wec_ebible/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:wec_ebible/pi1/flexform_ds.xml');
-
 t3lib_extMgm::addPlugin(Array('LLL:EXT:wec_ebible/locallang_db.xml:tt_content.list_type_pi2', $_EXTKEY.'_pi2'),'list_type');
 
 
-$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_wecebible_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_wecebible_pi1_wizicon.php';
 $TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_wecebible_pi2_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi2/class.tx_wecebible_pi2_wizicon.php';
-	
+
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/', 'WEC eBible');
 
 t3lib_div::loadTCA('fe_users');
